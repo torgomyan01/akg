f7f061cf3ccfb2e1a263901933a5f5293ba1b10e
torgomyan1998 <andranik.torgomyan1998@gmail.com>
2022-10-21 20:14:14 +0400

    Start Project AKG

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$('.header-products').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    nextArrow: `<button class="slider-buttons"><i class="fa-solid fa-angle-right"></i></button>`,
+    prevArrow: `<button class="slider-buttons left"><i class="fa-solid fa-angle-left"></i></button>`,
+});
